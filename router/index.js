431543b783aa8c5c44ca328b30256a0d3a21c1b4
wahyu <komang01966@gmail.com>
2022-10-01 12:27:00 +0800

    Update add todo endpoint

--- router/index.js
+++ router/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import TodoController from "../controllers/todoController.js";
 
 router.get("/todo", TodoController.getTodo);
-router.post("/todo/add", TodoController.createTodo);
+router.post("/todo", TodoController.createTodo);
 router.get("/todo/:id", TodoController.getById);
 router.put("/todo/:id", TodoController.updateTodo);
 router.delete("/todo/:id", TodoController.deleteTodo);
